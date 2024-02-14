fe3a0b01dd3c79c284a73ee1a41fea0b67b17e22
Dimbryan <l18370926@apizaco.tecnm.mx>
2024-02-14 10:43:58 -0600

    se agrega el proyecto

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camaras de video vigilancia</title>
+    </style>
+</head>
+
+<body>
+    <header>C5</header>
+        <section>
+            <h2>Sobre la Pagina</h2>
+            <p>Esta es una prueba de la Pagina de video vigilancia</p>
+            <p style="color: #B08EFF; font-size: 18px;"><?php echo date('d-m-y');  ?></p>
+        </section>
+    <ul>
+        <li>1080</li>
+        <li>1080</li>
+        <li>1080</li>
+        <li>1080</li>
+    </ul>
+
+    <h1 style>
+        <?php echo "hola soy bryan"; ?>
+    </h1>
+    <h1>
+        <?php 
+        $nombre = "Bryan";
+        $edad = 25;
+
+        echo "soy $nombre, y tengo $edad"; 
+        ?>
+    </h1>
+
+</body>
+</html>
+
+
