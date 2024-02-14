d16dbb12fd58e819df0fa934b689b9c8e92f064f
Dimbryan <l18370926@apizaco.tecnm.mx>
2024-02-14 13:53:36 -0600

    Se estructuro la pagina

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,34 +8,37 @@
 </head>
 
 <body>
-    <header>C5</header>
-        <section>
-            <h2>Sobre la Pagina</h2>
-            <p>Esta es una prueba de la Pagina de video vigilancia</p>
+    <header>
+        <div class="Search bar">
+            <input type="text" placeholder="buscar...">
+            <button type="submit">Buscar</button>
+        </div>
+    </header>
+    <nav>
+        <a href = "1">Inicio</a>
+        <a href = "2">acerca de mi </a>
+        <a href = "3">contacto</a>
+        <a href = "4">correo</a>
+    </nav>
             <p style="color: #B08EFF; font-size: 18px;"><?php echo date('d-m-y');  ?></p>
-        </section>
-    <ul>
-        <li>1080</li>
-        <li>1080</li>
-        <li>1080</li>
-        <li>1080</li>
-    </ul>
 
-    <h1 style>
-        <?php echo "hola soy bryan"; ?>
-    </h1>
-    <h1>
-        <?php 
-        $nombre = "Bryan";
-        $edad = 25;
-
-        echo "soy $nombre, y tengo $edad"; 
-        ?>
-    </h1>
     <section>
-        <h2>Se actualizo el repositorio</h2>
+        <p><strong>La transformación digital</strong> redefine la forma en que las empresas operan y se relacionan con sus clientes. Desde la implementación de soluciones basadas en la nube hasta la automatización de procesos, estamos en la vanguardia de una revolución que impulsa la eficiencia y la innovación.</p>
+        <p><strong>Inteligencia Artificial en Acción:</strong> Descubre cómo la inteligencia artificial está revolucionando sectores como la atención al cliente, la medicina y la logística. Desde chatbots inteligentes hasta sistemas de aprendizaje automático, estamos explorando el potencial ilimitado de la IA para mejorar la vida cotidiana.</p>
+        <p><strong>El Futuro de la Conectividad:</strong>La llegada de la tecnología 5G está transformando la conectividad, brindando velocidades de internet sin precedentes y permitiendo avances en áreas como vehículos autónomos y la Internet de las cosas (IoT). Descubre cómo estas innovaciones están dando forma al futuro digital.</p>
     </section>
-
+    <div class= "aditional info">
+        <aside>
+            <p>Informacion lateral adicional</p>
+        </aside>
+    </div>
+    <footer>
+        <nav>
+        <a href="T">Tiktok</a>
+        <a href="Y">Youtube</a>
+        <a href="F">Facebook</a>
+        </nav>
+    </footer>
 </body>
 </html>
 
